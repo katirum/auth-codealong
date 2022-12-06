d0ea7cf0e1d3b397a945a7a31e7125f600ccc260
katirum <kartikaprw@gmail.com>
2022-12-06 09:11:17 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import crypto from 'crypto'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt-nodejs'
 
+const mongoUrl = process.env.MONGO_URL|| "mongodb://localhost/auth"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: ()=> crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')});
+  if(user){
+    req.user = user;
+    next();
+  }else{
+    res.status(401).json({loggedOut:true})
+  }
+}
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -18,6 +53,30 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+app.post('/users', async (req, res) => {
+  try{
+    const {name, email, password} = req.body;
+    const user = new User({name, email, password: bcrypt.hashSync(password)});
+    user.save();
+    res.status(201).json({id: user._id, accessToken: user.accessToken});
+  }catch(err){
+    res.status(400).json({message: 'could not create user', errors: err.errors})
+  }
+})
+
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req, res) => {
+  res.json({secret: 'this is secret'})
+})
+
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({email: req.body.email});
+  if(user && bcrypt.compareSync(req.body.password, user.password)){
+    res.json({userId: user._id, accessToken: user.accessToken});
+  }else{
+    res.json({notFound: true})
+  }
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
